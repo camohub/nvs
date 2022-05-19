d257e7f61d27a96558ec4dd42f6213305c5b91e8
Čamaj Vladimír <vladimir.camaj@gmail.com>
2022-05-19 13:14:36 +0200

    Header text update

--- resources/views/layout-full.blade.php
+++ resources/views/layout-full.blade.php
@@ -15,7 +15,7 @@
 	<div id="header-wrapper" class="bS2">
 		<div class="container">
 			<div class="row oH">
-				<h1 id="header" class="col-12 oH translate translateInit">filip<span id="header-effect">sramko</span>.sk</h1>
+				<h1 id="header" class="col-12 oH translate translateInit">Filip<span id="header-effect">sramko</span>.sk</h1>
 			</div>
 		</div>
 	</div>
